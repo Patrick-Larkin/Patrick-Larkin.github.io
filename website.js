e9a12ab0a39a6b58c2a002f174c767e0ca7026c7
patrick <patricklarkin995@gmail.com>
2020-09-18 08:57:24 -0500

    once refactored personel website

--- website.js
+++ website.js
@@ -3,42 +3,44 @@ $(document).ready( function () {
 //Todo:Output phone number and email
 //Todo: Apply to icon
 
-// let countDownDate = new Date("Jan 20, 2021 15:37:25").getTime();
-//
-// // Update the count down every 1 second
-// 	let x = setInterval(function() {
-//
-// 		// Get today's date and time
-// 		let now = new Date().getTime();
-//
-// 		// Find the distance between now and the count down date
-// 		let distance = countDownDate - now;
-//
-// 		// Time calculations for days, hours, minutes and seconds
-// 		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-// 		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// 		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-// 		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//
-// 		// Output the result in an element with id="demo"
-// 		document.getElementById("timer").innerHTML =
-// 				'<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + days + ' D</div></span>'
-// 				+ '<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + hours + ' H</div></span>'
-// 				+ '<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + minutes + ' M</div></span>'
-// 				+ '<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + seconds + ' S</div> </span>'
-//
-// 		// If the count down is over, write some text
-// 		if (distance < 0) {
-// 			clearInterval(x);
-// 			document.getElementById("demo").innerHTML = "EXPIRED";
-// 		}
-// 	}, 1000);
-// 	function rnd (min, max) {
-// 		return Math.random() * (max - min) + min;
-// 	}
+	let countDownDate = new Date("Jan 20, 2021 15:37:25").getTime();
 
-	// let generateQuote = function (data) {
-	// 	$('#quote').html(data.quoteText);
-	// 	$('#author').html(data.quoteAuthor);
-	// 	$('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text=' + data.quoteText + '- ' + data.quoteAuthor).attr('target','_blank');
-	// }
+// Update the count down every 1 second
+	let x = setInterval(function () {
+
+		// Get today's date and time
+		let now = new Date().getTime();
+
+		// Find the distance between now and the count down date
+		let distance = countDownDate - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// Output the result in an element with id="demo"
+		document.getElementById("timer").innerHTML =
+				'<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + days + ' D</div></span>'
+				+ '<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + hours + ' H</div></span>'
+				+ '<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + minutes + ' M</div></span>'
+				+ '<div class="holder m-2 d-inline-flex"><span class="h1 font-weight-bold">' + seconds + ' S</div> </span>'
+
+		// If the count down is over, write some text
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("demo").innerHTML = "EXPIRED";
+		}
+	}, 1000);
+
+	$(function () {
+		$(window).on('scroll', function () {
+			if ( $(window).scrollTop() > 10 ) {
+				$('.navbar').addClass('active');
+			} else {
+				$('.navbar').removeClass('active');
+			}
+		});
+	});
+});
